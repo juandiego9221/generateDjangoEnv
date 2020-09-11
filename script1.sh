9964f84c1444d4279fdb685d5c96bda5ba37718a
juandiego <juandiego9221@gmail.com>
2020-09-10 22:47:48 -0500

    modificacion de script

--- script1.sh
+++ script1.sh
@@ -4,11 +4,19 @@ function prop {
   grep "${1}" main.properties|cut -d'=' -f2 
 }
 
+HORA_INICIO=$(date)
 destiny_path=$(prop 'destiny.path')
 dirname=$1
 dirname2=$2
 app=$3
 
+echo "============================================================"
+echo "             SCRIPT MAIN "
+echo "------------------------------------------------------------"
+echo "--------- INICIO DE EJECUCION DE SCRIPT --------------------"
+echo "--------- $HORA_INICIO ---------------------"
+echo "============================================================"
+echo -e "\n"
 if [ ! -d "$dirname" ]
 then
   echo " Directory does not exits. creating ..."
@@ -34,3 +42,9 @@ echo " Finish creating projectt"
 echo " Creatin  app ..."
 mkdir $destiny_path/$dirname/$dirname2/$app
 django-admin startapp $app $destiny_path/$dirname/$dirname2/$app
+
+echo -e "\n"
+echo "============================================================"
+echo "--------- FIN DE EJECUCION DE SCRIPT -----------------------"
+echo "--------- $HORA_FIN ---------------------"
+echo "============================================================"
